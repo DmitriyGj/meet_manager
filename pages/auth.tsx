ead9d5050474703b8fa768dedbc4720b9cf9c904
DmitriyGj <71816721+DmitriyGj@users.noreply.github.com>
2022-03-29 23:49:48 +0500

    try to fix deploy

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,18 +1,10 @@
 import type { NextPage } from 'next'
 import styles from '../styles/Home.module.css'
-import { DataGrid, GridColDef, GridRowsProp } from '@mui/x-data-grid'
-import Button  from '@mui/material/Button'
 
 
 const Meetings: NextPage = () => {
     return (<div className={styles.container}>
-            <h1>Встречи</h1>
-            <DataGrid rows={rows} columns={columns} />
-            <div className = {styles.ButtonContainer}>
-                <Button variant='contained' sx = { {width:'10%' } }>Добавить</Button>
-                <Button variant='contained' sx = { {width:'10%' } }>Удалить</Button>
-                <Button variant='contained' sx = { {width:'10%' } } >Изменить</Button>
-                </div>
+            <h1>Auth</h1>
         </div>
     )
 }

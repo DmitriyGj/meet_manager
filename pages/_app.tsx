879803fa78e3c54acedbe732dd424b70786e0a20
DmitriyGj <71816721+DmitriyGj@users.noreply.github.com>
2022-05-10 15:55:32 +0500

    asd

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,18 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { withLayout } from '../public/src/HOC/Layout/Layout';
+import { CacheProvider } from '@emotion/react';
+import createCache from '@emotion/cache';
+
+const cache = createCache({
+  key: 'css',
+  prepend: true,
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return ( <CacheProvider value={cache}>
+          <Component {...pageProps} />
+    </CacheProvider>)
 }
 
 export default withLayout(MyApp)

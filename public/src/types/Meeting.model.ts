005d02b85d9423af526a0ce611d08d6741881174
DmitriyGj <71816721+DmitriyGj@users.noreply.github.com>
2022-05-21 01:14:18 +0500

    asd

--- public/src/types/Meeting.model.ts
+++ public/src/types/Meeting.model.ts
@@ -1,7 +1,7 @@
 export default interface IMeeting {
     ID: string | never ;
-    START_DATE: Date;
-    END_DATE: Date
+    START_DATE: Date | number;
+    END_DATE: Date | number
     INICIATOR_ID:string | never
     MEMBERS: string[] | never[]
     GUESTS: string[] | never[]
